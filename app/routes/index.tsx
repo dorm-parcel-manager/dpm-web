3ba4931709d870138c9080bc07c42e2224966535
Suphon Thanakornpakapong <paphonb@gmail.com>
2022-10-05 16:33:57 +0700

    Add layout to login page

--- app/routes/index.tsx
+++ app/routes/index.tsx
@@ -1,50 +1,5 @@
-import { Button, Typography } from "@mui/joy";
-import { Stack } from "@mui/system";
-import type { LoaderFunction } from "@remix-run/node";
-import { json } from "@remix-run/node";
-import { Link, useLoaderData } from "@remix-run/react";
-import { userServiceClient } from "~/client";
-import type { HelloResponse } from "~/proto/common";
-import { useState } from "react";
-import { FadeModal } from "~/components/FadeModal";
-import { ModeToggle } from "~/components/ModeToggle";
-
-type LoaderData = HelloResponse;
-
-export const loader: LoaderFunction = async () => {
-  const result = await userServiceClient.hello({ name: "Remix" });
-  return json<LoaderData>(result.response);
-};
+import Login from "~/routes/login";
 
 export default function Index() {
-  const data = useLoaderData<LoaderData>();
-  const [open, setOpen] = useState(false);
-
-  return (
-    <Stack alignItems="start">
-      <Typography level="h4" component="h1">
-        {data.message}
-      </Typography>
-      <Button component={Link} to="/login">
-        Login
-      </Button>
-      <Button onClick={() => setOpen(true)}>Open Modal</Button>
-      <ModeToggle />
-      <FadeModal open={open} onClose={() => setOpen(false)}>
-        <div>
-          <Typography
-            component="h2"
-            level="inherit"
-            fontSize="1.25em"
-            mb="0.25em"
-          >
-            Transition modal
-          </Typography>
-          <Typography textColor="text.tertiary">
-            Using `react-transition-group` to create a fade animation.
-          </Typography>
-        </div>
-      </FadeModal>
-    </Stack>
-  );
+  return <Login />;
 }
--- app/routes/login.tsx
+++ app/routes/login.tsx
@@ -1,10 +1,66 @@
-import { Button } from "@mui/joy";
+import {
+  Button,
+  Container as JoyContainer,
+  styled,
+  Typography,
+} from "@mui/joy";
+import { Stack } from "@mui/system";
+
+import logo from "~/assets/images/logo.svg";
+import googleLogo from "~/assets/images/google-logo.svg";
 import { Form } from "@remix-run/react";
+
+const Container = styled(JoyContainer)(({ theme }) => ({
+  paddingTop: 64,
+  "&": {
+    maxWidth: 400,
+  },
+  [theme.breakpoints.up("sm")]: {
+    paddingTop: 96,
+  },
+}));
+
+const LogoImg = styled("img")({
+  display: "inline-block",
+  width: 60,
+  height: 60,
+  marginRight: 16,
+  marginBottom: 32,
+});
+
+const GoogleLogoImg = styled("img")({
+  marginRight: "0.75rem",
+  width: 18,
+  height: 18,
+});
 
 export default function Login() {
   return (
-    <Form action="/auth/google" method="post">
-      <Button type="submit">Login with Google</Button>
-    </Form>
+    <Container>
+      <Stack>
+        <LogoImg src={logo} alt="" />
+        <Typography level="h2">Dorm Parcel Manager</Typography>
+      </Stack>
+      <Typography
+        level="body2"
+        sx={(theme) => ({
+          fontSize: theme.typography.h4.fontSize,
+          lineHeight: 1.2,
+          marginTop: 1,
+        })}
+      >
+        A parcel management system for dormitory students
+      </Typography>
+      <Form action="/auth/google" method="post">
+        <Button
+          type="submit"
+          variant="outlined"
+          color="neutral"
+          sx={{ marginTop: 4 }}
+        >
+          <GoogleLogoImg src={googleLogo} alt="" /> Sign In with Google
+        </Button>
+      </Form>
+    </Container>
   );
 }
