25249b2fbdba57437077686cd7bb40c2ed434294
Suphon Thanakornpakapong <paphonb@gmail.com>
2022-10-05 18:11:04 +0700

    Set site title

--- app/root.tsx
+++ app/root.tsx
@@ -20,7 +20,7 @@ import { GOOGLE_CLIENT_ID } from "./env";
 
 export const meta: MetaFunction = () => ({
   charset: "utf-8",
-  title: "New Remix App",
+  title: "Dorm Parcel Manager",
   viewport: "width=device-width,initial-scale=1",
 });
 
