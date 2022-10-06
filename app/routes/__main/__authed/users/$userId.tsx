65e6537f33194930b4fea1e96853ae24f986eb4a
Suphon Thanakornpakapong <paphonb@gmail.com>
2022-10-06 17:02:49 +0700

    Implement deleting user

--- app/routes/__main/__authed/users/$userId.tsx
+++ app/routes/__main/__authed/users/$userId.tsx
@@ -1,12 +1,22 @@
 import { Avatar, Box, Button, Grid, Typography } from "@mui/joy";
-import type { LoaderArgs } from "@remix-run/node";
+import type { ActionArgs, LoaderArgs } from "@remix-run/node";
+import { redirect } from "@remix-run/node";
 import { json } from "@remix-run/node";
-import { Link, useLoaderData, useNavigate } from "@remix-run/react";
+import {
+  Link,
+  useLoaderData,
+  useNavigate,
+  useSubmit,
+  useTransition,
+} from "@remix-run/react";
 import { format } from "date-fns";
+
 import { MdArrowBackIosNew, MdDelete, MdEdit } from "react-icons/md";
 import { getGrpcContext } from "~/auth/utils";
 import { userServiceClient } from "~/client";
 import { AvatarInitials } from "~/components/AvatarInitials";
+import { FadeModal } from "~/components/FadeModal";
+import { useDisclosure } from "~/hooks/useDisclosure";
 import { UserType } from "~/proto/common";
 import { Timestamp } from "~/proto/google/protobuf/timestamp";
 import type { User } from "~/proto/user-service";
@@ -21,13 +31,49 @@ export async function loader({ request, params }: LoaderArgs) {
   return json<LoaderData>(user);
 }
 
+export async function action({ request, params }: ActionArgs) {
+  switch (request.method) {
+    case "DELETE": {
+      const context = await getGrpcContext(request);
+      const id = parseInt(params.userId as string);
+      await userServiceClient.deleteUser({ context, id }).response;
+      return redirect("/users");
+    }
+    default: {
+      throw new Response("Method Not Allowed", {
+        status: 405,
+        statusText: "Method Not Allowed",
+      });
+    }
+  }
+}
+
 export default function UserDetail() {
   const user = useLoaderData<LoaderData>();
   const navigate = useNavigate();
+  const deleteModal = useDisclosure();
+  const submit = useSubmit();
+  const transition = useTransition();
+  const isLoading = transition.state !== "idle";
+
+  const onCancelDelete = () => {
+    if (isLoading) return;
+    deleteModal.onClose();
+  };
+
+  const onConfirmDelete = () => {
+    submit({}, { method: "delete" });
+  };
+
   return (
     <div>
       <Box sx={{ display: "flex", alignItems: "baseline" }}>
-        <Button onClick={() => navigate(-1)} size="sm" sx={{ marginRight: 2 }}>
+        <Button
+          onClick={() => navigate(-1)}
+          size="sm"
+          disabled={isLoading}
+          sx={{ marginRight: 2 }}
+        >
           <MdArrowBackIosNew />
         </Button>
         <Typography level="h4" sx={{ display: { xs: "none", md: "block" } }}>
@@ -39,12 +85,19 @@ export default function UserDetail() {
           to="edit"
           color="info"
           size="sm"
+          disabled={isLoading}
           startDecorator={<MdEdit />}
           sx={{ marginRight: 2 }}
         >
           Edit
         </Button>
-        <Button color="danger" size="sm" startDecorator={<MdDelete />}>
+        <Button
+          onClick={deleteModal.onOpen}
+          color="danger"
+          size="sm"
+          disabled={isLoading}
+          startDecorator={<MdDelete />}
+        >
           Delete
         </Button>
       </Box>
@@ -91,6 +144,34 @@ export default function UserDetail() {
           </Typography>
         </Grid>
       </Grid>
+      <FadeModal open={deleteModal.isOpen} onClose={onCancelDelete}>
+        <div>
+          <Typography level="h4">Delete User</Typography>
+          <Typography sx={{ marginTop: 2 }}>
+            Are you sure you want to delete this user?
+          </Typography>
+          <Box
+            sx={{ display: "flex", justifyContent: "flex-end", marginTop: 2 }}
+          >
+            <Button
+              variant="soft"
+              color="neutral"
+              onClick={onCancelDelete}
+              disabled={isLoading}
+              sx={{ marginRight: 2 }}
+            >
+              Cancel
+            </Button>
+            <Button
+              color="danger"
+              onClick={onConfirmDelete}
+              disabled={isLoading}
+            >
+              Delete
+            </Button>
+          </Box>
+        </div>
+      </FadeModal>
     </div>
   );
 }
