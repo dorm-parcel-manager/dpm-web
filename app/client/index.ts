6d31662821f1b37e38cc52648638ff08128ecc78
Suphon Thanakornpakapong <paphonb@gmail.com>
2022-10-14 16:58:04 +0700

    Add parcel service client

--- app/client/index.ts
+++ app/client/index.ts
@@ -1,13 +1,21 @@
 import { GrpcTransport } from "@protobuf-ts/grpc-transport";
-import { CLIENT_USERSERVICEURL } from "~/env";
+import { CLIENT_PARCELSERVICEURL, CLIENT_USERSERVICEURL } from "~/env";
 import { ChannelCredentials } from "@grpc/grpc-js";
 import { UserServiceClient } from "~/proto/user-service-client";
+import { ParcelServiceClient } from "~/proto/parcel-service-client";
 
 export * from "./utils";
 
 export const userServiceClient = new UserServiceClient(
   new GrpcTransport({
     host: CLIENT_USERSERVICEURL,
+    channelCredentials: ChannelCredentials.createInsecure(),
+  })
+);
+
+export const parcelServiceClient = new ParcelServiceClient(
+  new GrpcTransport({
+    host: CLIENT_PARCELSERVICEURL,
     channelCredentials: ChannelCredentials.createInsecure(),
   })
 );
--- app/env/index.ts
+++ app/env/index.ts
@@ -1,5 +1,7 @@
 export const CLIENT_USERSERVICEURL = process.env
   .CLIENT_USERSERVICEURL as string;
+export const CLIENT_PARCELSERVICEURL = process.env
+  .CLIENT_PARCELSERVICEURL as string;
 export const GOOGLE_CLIENT_ID = process.env.GOOGLE_CLIENT_ID as string;
 export const GOOGLE_CLIENT_SECRET = process.env.GOOGLE_CLIENT_SECRET as string;
 export const BASE_URL = process.env.BASE_URL as string;
