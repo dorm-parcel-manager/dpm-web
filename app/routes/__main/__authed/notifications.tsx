64d03755c2059b9445d958a985465d917c191a1b
Chantawat Yangtrong <cyredhat@gmail.com>
2022-10-20 21:07:55 +0700

    Add notification link

--- app/routes/__main/__authed/notifications.tsx
+++ app/routes/__main/__authed/notifications.tsx
@@ -1,23 +1,11 @@
-import { Divider, Typography } from "@mui/joy";
-import type { LoaderFunction } from "@remix-run/node";
-import { useLoaderData } from "@remix-run/react";
-import { authenticator } from "~/services/auth.server";
+import { Divider, List, ListItem, ListItemButton, Typography } from "@mui/joy";
+import { redirect } from "@remix-run/node";
+import { useLoaderData, useSubmit } from "@remix-run/react";
 
 import { formatRelative } from "date-fns";
+import { getUser } from "~/auth/utils";
 
-export const loader: LoaderFunction = async ({ request }) => {
-  const user = await authenticator.isAuthenticated(request, {
-    failureRedirect: "/login",
-  });
-  const notificationServiceURL = process.env.CLIENT_NOTIFICATIONSERVICEURL!
-  const response = await fetch(notificationServiceURL + '/notification', {
-    headers: {
-      "User-Id": user.id.toString(),
-    },
-  })
-  const notifications: Notification[] = await response.json()
-  return notifications.sort((a, b) => b.unixTime - a.unixTime);
-};
+import type { ActionFunction, LoaderFunction } from "@remix-run/node";
 
 interface Notification {
   _id: string;
@@ -29,8 +17,49 @@ interface Notification {
   unixTime: number;
 }
 
+export const loader: LoaderFunction = async ({ request }) => {
+  const user = await getUser(request)
+  const notificationServiceURL = process.env.CLIENT_NOTIFICATIONSERVICEURL!
+  const response = await fetch(notificationServiceURL + '/notification', {
+    headers: {
+      "User-Id": user.id.toString(),
+    },
+  })
+  const notifications: Notification[] = await response.json()
+  return notifications.sort((a, b) => b.unixTime - a.unixTime);
+};
+
+export const action: ActionFunction = async ({ request }) => {
+  const user = await getUser(request)
+  const notificationServiceURL = process.env.CLIENT_NOTIFICATIONSERVICEURL!
+  const formData = await request.formData();
+  const id = formData.get("id") as string;
+  const link = formData.get("link") as string;
+  await fetch(notificationServiceURL + '/notification/' + id, {
+    method: "PATCH",
+    headers: {
+      "User-Id": user.id.toString(),
+    },
+    body: JSON.stringify({
+      read: true,
+    })
+  })
+  return redirect(link)
+}
+
 export default function Notifications() {
   const notifications = useLoaderData<Notification[]>();
+  const submit = useSubmit();
+
+  const handleClick = (notification: Notification) => {
+    const data = new FormData();
+    data.append("id", notification._id);
+    data.append("link", notification.link);
+    submit(data, {
+      method: "post",
+      action: `/notifications`,
+    });
+  }
 
   return (
     <div>
@@ -41,32 +70,40 @@ export default function Notifications() {
         rowGap: '0.5rem',
         padding: '1rem 0',
       }}>
+      <List>
       {
         notifications.map((notification) => (
-          <>
-            <div key={notification._id}>
-              {
-                notification.read ? null :(
-                  <div style={{
-                    position: "absolute",
-                    height: "8px",
-                    width: "8px",
-                    marginLeft: "-0.7rem",
-                    marginTop: "0.6rem",
-                    backgroundColor: "#0BA5EC",
-                    borderRadius: "50%",
-                    display: "inline-block",
-                  }} />
-                )
-              }
-              
-              <Typography level="h6">{notification.title}</Typography>
-              <Typography level="body2">{formatRelative(new Date(notification.unixTime), new Date())}</Typography>
-            </div>
+          <ListItem key={notification._id} >
+            <ListItemButton
+              onClick={() => {handleClick(notification)}}
+              sx={{
+                marginLeft: "-1.5rem",
+              }}>
+              <div>
+                {
+                  notification.read ? null :(
+                    <div style={{
+                      position: "absolute",
+                      height: "8px",
+                      width: "8px",
+                      marginLeft: "-0.7rem",
+                      marginTop: "0.6rem",
+                      backgroundColor: "#0BA5EC",
+                      borderRadius: "50%",
+                      display: "inline-block",
+                    }} />
+                  )
+                }
+                
+                <Typography level="h6">{notification.title}</Typography>
+                <Typography level="body2">{formatRelative(new Date(notification.unixTime), new Date())}</Typography>
+              </div>
             <Divider />
-          </>
+           </ListItemButton>
+          </ListItem>
         ))
       }
+      </List>
       </div>
     </div>
   );
