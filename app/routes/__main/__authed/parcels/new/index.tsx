d3a5f7fb5dadaf3ff19ae679010b4932b723a1ce
Watwe <anon.ongsakul2000@gmail.com>
2022-10-20 15:40:26 +0700

    add parcel field validation

--- app/routes/__main/__authed/parcels/new/index.tsx
+++ app/routes/__main/__authed/parcels/new/index.tsx
@@ -1,15 +1,21 @@
 import { Box, Button, Typography, TextField, Stack } from "@mui/joy";
-import { ActionArgs, redirect } from "@remix-run/node";
-import { Link, SubmitFunction, useNavigate, useSubmit } from "@remix-run/react";
-import { useState } from "react";
+import { ActionArgs, json, redirect } from "@remix-run/node";
+import { Link, SubmitFunction, useActionData, useSubmit } from "@remix-run/react";
+import { useEffect, useState } from "react";
 import { AiFillCaretLeft } from "react-icons/ai";
 import { FiSave } from "react-icons/fi";
 import { getGrpcContext } from "~/auth/utils";
 import { parcelServiceClient } from "~/client";
 
+function validate(data: string|undefined, name: string){
+    if (!data) {
+        return name+" is required";
+    } else if (typeof data !== "string" || data.length < 0) {
+        return name+` must not be empty`;
+    }
+}
 
-
-export async function action({ request }: ActionArgs) : Promise<boolean> {
+export async function action({ request }: ActionArgs) {
     const context = await getGrpcContext(request);
 
     const form = await request.formData();
@@ -18,83 +24,96 @@ export async function action({ request }: ActionArgs) : Promise<boolean> {
     const trackingNumber = form.get('trackingNumber')?.toString();
     const transportCompany = form.get('transportCompany')?.toString();
     const sender = form.get('sender')?.toString();
-    if(name && trackingNumber && transportCompany && sender){
-        const data = {ownerId, name, transportCompany, trackingNumber, sender};
-        const { response, status } = await parcelServiceClient.createParcel({ context, data })
-        return true
+
+    const errors = {
+        name: validate(name, "Parcel name"),
+        trackingNumber: validate(trackingNumber, "Tracking Number"),
+        transportCompany: validate(transportCompany, "Transport Company"),
+        sender: validate(sender, "Sender"),
+    };
+
+    if (ownerId && name && trackingNumber && transportCompany && sender &&
+        name !=="" && trackingNumber !=="" && transportCompany !=="" && sender !=="") {
+        const data = { ownerId, name, transportCompany, trackingNumber, sender };
+        await parcelServiceClient.createParcel({ context, data })
+        return redirect(`/parcels`);
     }
-    return false
+
+    return {errors}
 }
 
 export default function Parcels() {
     const submit = useSubmit();
+
     return (
         <>
-        <Box sx={{ display: "flex", alignItems: "baseline" }}>
-            <Button
-                component={Link}
-                to="/parcels"
-                color="primary"
-                size="sm"
-                sx={{ mr: 2 }}
-                startDecorator={<AiFillCaretLeft />}
-            />
-            <Typography level="h4">New Parcel</Typography>
-        </Box>
-        <NewParcelView submit={submit}/>
+            <Box sx={{ display: "flex", alignItems: "baseline" }}>
+                <Button
+                    component={Link}
+                    to="/parcels"
+                    color="primary"
+                    size="sm"
+                    sx={{ mr: 2 }}
+                    startDecorator={<AiFillCaretLeft />}
+                />
+                <Typography level="h4">New Parcel</Typography>
+            </Box>
+            <NewParcelView submit={submit} />
         </>
     );
 }
 
-function NewParcelView({submit} : {submit : SubmitFunction}) {
+function NewParcelView({ submit }: { submit: SubmitFunction }) {
     const [name, setName] = useState<string>("");
     const [trackingNumber, setTrackingNumber] = useState<string>("");
     const [transportCompany, setTransportCompany] = useState<string>("");
     const [sender, setSender] = useState<string>("");
-    const [link, setLink] = useState<boolean>(false);
+    const actionData = useActionData();
 
     return (
         <Stack direction="column" spacing={3} sx={{ marginTop: 2 }}>
             <TextField
-                label = "Parcel Name"
-                placeholder = "iPhone 14 Pro Max"
-                variant="outlined"
+                label="Parcel Name"
+                placeholder={actionData?.errors?.name ? actionData?.errors?.name:"iPhone 14 Pro Max"}
+                color={actionData?.errors?.name ? "warning":"neutral"}
+                variant= "outlined"
                 value={name}
-                onChange={(newValue) => {setName(newValue.target.value)}}
+                onChange={(newValue) => { setName(newValue.target.value) }}
             />
             <TextField
-                label = "Tracking Number"
-                placeholder = "AB01234567890"
+                label="Tracking Number"
+                placeholder={actionData?.errors?.trackingNumber ? actionData?.errors?.trackingNumber:"AB01234567890"}
+                color={actionData?.errors?.trackingNumber ? "warning":"neutral"}
                 variant="outlined"
                 value={trackingNumber}
-                onChange={(newValue) => {setTrackingNumber(newValue.target.value)}}
+                onChange={(newValue) => { setTrackingNumber(newValue.target.value) }}
             />
             <TextField
-                label = "Transport Company"
-                placeholder = "The Flash"
+                label="Transport Company"
+                placeholder={actionData?.errors?.transportCompany ? actionData?.errors?.transportCompany:"The Flash"}
+                color={actionData?.errors?.transportCompany ? "warning":"neutral"}
                 variant="outlined"
                 value={transportCompany}
-                onChange={(newValue) => {setTransportCompany(newValue.target.value)}}
+                onChange={(newValue) => { setTransportCompany(newValue.target.value) }}
             />
             <TextField
-                label = "Sender"
-                placeholder = "Anos Positos"
+                label="Sender"
+                placeholder={actionData?.errors?.sender ? actionData?.errors?.sender:"Anos Positos"}
+                color={actionData?.errors?.sender ? "warning":"neutral"}
                 variant="outlined"
                 value={sender}
-                onChange={(newValue) => {setSender(newValue.target.value)}}
+                onChange={(newValue) => { setSender(newValue.target.value) }}
             />
             <Box textAlign='center'>
                 <Button
-                    component={Link}
-                    to="/parcels"
                     color="info"
                     size="sm"
-                    sx={{ mr: 2, width: 300}} 
+                    sx={{ mr: 2, width: 300 }}
                     onClick={() => {
                         submit({ name, transportCompany, trackingNumber, sender }, { method: "post" })
                     }}
                 >
-                    <FiSave style={{ marginRight: 2 }}/>
+                    <FiSave style={{ marginRight: 2 }} />
                     <Typography>Save</Typography>
                 </Button>
             </Box>
