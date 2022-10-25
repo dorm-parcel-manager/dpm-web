2e2efed604367eb1df006c40ac25c7c7ef23890b
Anon-136 <anonjin44@gmail.com>
2022-10-25 13:43:53 +0700

    Format code

--- app/routes/__main/__authed/parcels/$parcelId/index.tsx
+++ app/routes/__main/__authed/parcels/$parcelId/index.tsx
@@ -14,11 +14,12 @@ import type { Parcel } from "~/proto/parcel-service";
 type LoaderData = Parcel;
 
 export async function loader({ request, params }: LoaderArgs) {
-    const id = parseInt(params.parcelId as string);
-    const context = await getGrpcContext(request);
-    const response = await parcelServiceClient.getParcel({ context, id }).response;
-    const parcel = response.parcel!;
-    return json<LoaderData>(parcel);
+  const id = parseInt(params.parcelId as string);
+  const context = await getGrpcContext(request);
+  const response = await parcelServiceClient.getParcel({ context, id })
+    .response;
+  const parcel = response.parcel!;
+  return json<LoaderData>(parcel);
 }
 
 export default function Parcels() {
@@ -28,30 +29,30 @@ export default function Parcels() {
     <div>
       <Box sx={{ display: "flex", alignItems: "baseline" }}>
         <Button
-            component={Link}
-            to="/parcels"
-            color="primary"
-            size="sm"
-            sx={{ mr: 2 }}
-            startDecorator={<AiFillCaretLeft />}
+          component={Link}
+          to="/parcels"
+          color="primary"
+          size="sm"
+          sx={{ mr: 2 }}
+          startDecorator={<AiFillCaretLeft />}
         />
         <Typography level="h4">Parcel</Typography>
       </Box>
-      <ParcelDetailView parcel = {parcel}/>
+      <ParcelDetailView parcel={parcel} />
     </div>
   );
 }
 
 function ParcelDetailView({ parcel }: { parcel: Parcel }) {
-    return (
-        <Stack direction="column" spacing={2} sx={{ marginTop: 2 }}>
-            <ParcelDetailCard parcel={parcel} />
-        </Stack>
-    );
+  return (
+    <Stack direction="column" spacing={2} sx={{ marginTop: 2 }}>
+      <ParcelDetailCard parcel={parcel} />
+    </Stack>
+  );
 }
 
-function ParcelDetailCard({ parcel }: { parcel: Parcel }){
-    return(
+function ParcelDetailCard({ parcel }: { parcel: Parcel }) {
+  return (
     <Card
       variant="outlined"
       sx={{
@@ -61,83 +62,64 @@ function ParcelDetailCard({ parcel }: { parcel: Parcel }){
         },
       }}
     >
-        <Stack>
-            <Typography
-            level="h4"
-            textColor="text.primary"
-            >
-            { parcel.name ? parcel.name : 'iPhone 15 Pro max'}
-            </Typography>
-            <Typography
+      <Stack>
+        <Typography level="h4" textColor="text.primary">
+          {parcel.name ? parcel.name : "iPhone 15 Pro max"}
+        </Typography>
+        <Typography
+          level="body2"
+          textColor="text.primary"
+          sx={{ marginRight: 0.5 }}
+        >
+          from {parcel.sender}
+        </Typography>
+        <Box sx={{ display: "flex" }}>
+          <Typography
             level="body2"
             textColor="text.primary"
             sx={{ marginRight: 0.5 }}
-            >
-            from {parcel.sender}
-            </Typography>
-            <Box sx={{ display: "flex"}}>
-                <Typography
-                level="body2"
-                textColor="text.primary"
-                sx={{ marginRight: 0.5 }}
-                >
-                via {parcel.transportCompany} - {parcel.trackingNumber}
-                </Typography>
-                <CopyButton />
-            </Box>
-            {
-                parcel.description &&
-                <Typography
-                    level="body2"
-                    textColor="text.primary"
-                    sx={{ marginRight: 0.5 }}
-                    >
-                    Description : {" "}{parcel.description}
-                </Typography>
-            }
-        </Stack>
-        <Box sx={{marginTop : 1.5 }}>
-            {parcel.createdAt && 
-                <Typography
-                level="h6"
-                textColor="text.primary"
-                >
-                Registered : 
-                {" "+formatRelative(
-                    Timestamp.toDate(parcel.createdAt),
-                    new Date()
-                )}
-                </Typography>
-            }
-            {parcel.arrivalDate && 
-                <Typography
-                level="h6"
-                textColor="text.primary"
-                >
-                Arrived : 
-                {" "+formatRelative(
-                    Timestamp.toDate(parcel.arrivalDate),
-                    new Date()
-                )}
-                </Typography>
-            }
-            {parcel.pickedUpDate && 
-                <Typography
-                level="h6"
-                textColor="text.primary"
-                >
-                Picked up : 
-                {" "+formatRelative(
-                    Timestamp.toDate(parcel.pickedUpDate),
-                    new Date()
-                )}
-                </Typography>
-            }
+          >
+            via {parcel.transportCompany} - {parcel.trackingNumber}
+          </Typography>
+          <CopyButton />
         </Box>
+        {parcel.description && (
+          <Typography
+            level="body2"
+            textColor="text.primary"
+            sx={{ marginRight: 0.5 }}
+          >
+            Description : {parcel.description}
+          </Typography>
+        )}
+      </Stack>
+      <Box sx={{ marginTop: 1.5 }}>
+        {parcel.createdAt && (
+          <Typography level="h6" textColor="text.primary">
+            Registered :
+            {" " +
+              formatRelative(Timestamp.toDate(parcel.createdAt), new Date())}
+          </Typography>
+        )}
+        {parcel.arrivalDate && (
+          <Typography level="h6" textColor="text.primary">
+            Arrived :
+            {" " +
+              formatRelative(Timestamp.toDate(parcel.arrivalDate), new Date())}
+          </Typography>
+        )}
+        {parcel.pickedUpDate && (
+          <Typography level="h6" textColor="text.primary">
+            Picked up :
+            {" " +
+              formatRelative(Timestamp.toDate(parcel.pickedUpDate), new Date())}
+          </Typography>
+        )}
+      </Box>
 
-        <Box sx={{marginTop : 1.5}}>
-            <ParcelProgress status={parcel.status} />
-        </Box> 
+      <Box sx={{ marginTop: 1.5 }}>
+        <ParcelProgress status={parcel.status} />
+      </Box>
     </Card>
-    )
+  );
 }
--- app/routes/__main/__authed/parcels/new/index.tsx
+++ app/routes/__main/__authed/parcels/new/index.tsx
@@ -1,121 +1,149 @@
 import { Box, Button, Typography, TextField, Stack } from "@mui/joy";
-import { ActionArgs, redirect } from "@remix-run/node";
-import { Link, SubmitFunction, useActionData, useSubmit } from "@remix-run/react";
+import type { ActionArgs } from "@remix-run/node";
+import { redirect } from "@remix-run/node";
+import type { SubmitFunction } from "@remix-run/react";
+import { Link, useActionData, useSubmit } from "@remix-run/react";
 import { useState } from "react";
 import { AiFillCaretLeft } from "react-icons/ai";
 import { FiSave } from "react-icons/fi";
 import { getGrpcContext } from "~/auth/utils";
 import { parcelServiceClient } from "~/client";
 
-function validate(data: string|undefined, name: string){
-    if (!data) {
-        return name+" is required";
-    } else if (typeof data !== "string" || data.length < 0) {
-        return name+` must not be empty`;
-    }
+function validate(data: string | undefined, name: string) {
+  if (!data) {
+    return name + " is required";
+  } else if (typeof data !== "string" || data.length < 0) {
+    return name + ` must not be empty`;
+  }
 }
 
 export async function action({ request }: ActionArgs) {
-    const context = await getGrpcContext(request);
+  const context = await getGrpcContext(request);
 
-    const form = await request.formData();
-    const ownerId = context.userId;
-    const name = form.get('name')?.toString();
-    const trackingNumber = form.get('trackingNumber')?.toString();
-    const transportCompany = form.get('transportCompany')?.toString();
-    const sender = form.get('sender')?.toString();
+  const form = await request.formData();
+  const ownerId = context.userId;
+  const name = form.get("name")?.toString();
+  const trackingNumber = form.get("trackingNumber")?.toString();
+  const transportCompany = form.get("transportCompany")?.toString();
+  const sender = form.get("sender")?.toString();
 
-    const errors = {
-        name: validate(name, "Parcel name"),
-        trackingNumber: validate(trackingNumber, "Tracking Number"),
-        transportCompany: validate(transportCompany, "Transport Company"),
-        sender: validate(sender, "Sender"),
-    };
-    
-    if (Object.values(errors).some(Boolean)) return { errors };
-    if (ownerId && name && transportCompany && trackingNumber && sender){
-        const data = { ownerId, name, transportCompany, trackingNumber, sender };
-        await parcelServiceClient.createParcel({ context, data })
-    }
-    return redirect(`/parcels`);
+  const errors = {
+    name: validate(name, "Parcel name"),
+    trackingNumber: validate(trackingNumber, "Tracking Number"),
+    transportCompany: validate(transportCompany, "Transport Company"),
+    sender: validate(sender, "Sender"),
+  };
+
+  if (Object.values(errors).some(Boolean)) return { errors };
+  if (ownerId && name && transportCompany && trackingNumber && sender) {
+    const data = { ownerId, name, transportCompany, trackingNumber, sender };
+    await parcelServiceClient.createParcel({ context, data });
+  }
+  return redirect(`/parcels`);
 }
 
 export default function Parcels() {
-    const submit = useSubmit();
+  const submit = useSubmit();
 
-    return (
-        <>
-            <Box sx={{ display: "flex", alignItems: "baseline" }}>
-                <Button
-                    component={Link}
-                    to="/parcels"
-                    color="primary"
-                    size="sm"
-                    sx={{ mr: 2 }}
-                    startDecorator={<AiFillCaretLeft />}
-                />
-                <Typography level="h4">New Parcel</Typography>
-            </Box>
-            <NewParcelView submit={submit} />
-        </>
-    );
+  return (
+    <>
+      <Box sx={{ display: "flex", alignItems: "baseline" }}>
+        <Button
+          component={Link}
+          to="/parcels"
+          color="primary"
+          size="sm"
+          sx={{ mr: 2 }}
+          startDecorator={<AiFillCaretLeft />}
+        />
+        <Typography level="h4">New Parcel</Typography>
+      </Box>
+      <NewParcelView submit={submit} />
+    </>
+  );
 }
 
 function NewParcelView({ submit }: { submit: SubmitFunction }) {
-    const [name, setName] = useState<string>("");
-    const [trackingNumber, setTrackingNumber] = useState<string>("");
-    const [transportCompany, setTransportCompany] = useState<string>("");
-    const [sender, setSender] = useState<string>("");
-    const actionData = useActionData();
+  const [name, setName] = useState<string>("");
+  const [trackingNumber, setTrackingNumber] = useState<string>("");
+  const [transportCompany, setTransportCompany] = useState<string>("");
+  const [sender, setSender] = useState<string>("");
+  const actionData = useActionData();
 
-    return (
-        <Stack direction="column" spacing={3} sx={{ marginTop: 2 }}>
-            <TextField
-                label="Parcel Name"
-                placeholder={actionData?.errors?.name ? actionData?.errors?.name:"iPhone 14 Pro Max"}
-                color={actionData?.errors?.name ? "warning":"neutral"}
-                variant= "outlined"
-                value={name}
-                onChange={(newValue) => { setName(newValue.target.value) }}
-            />
-            <TextField
-                label="Tracking Number"
-                placeholder={actionData?.errors?.trackingNumber ? actionData?.errors?.trackingNumber:"AB01234567890"}
-                color={actionData?.errors?.trackingNumber ? "warning":"neutral"}
-                variant="outlined"
-                value={trackingNumber}
-                onChange={(newValue) => { setTrackingNumber(newValue.target.value) }}
-            />
-            <TextField
-                label="Transport Company"
-                placeholder={actionData?.errors?.transportCompany ? actionData?.errors?.transportCompany:"The Flash"}
-                color={actionData?.errors?.transportCompany ? "warning":"neutral"}
-                variant="outlined"
-                value={transportCompany}
-                onChange={(newValue) => { setTransportCompany(newValue.target.value) }}
-            />
-            <TextField
-                label="Sender"
-                placeholder={actionData?.errors?.sender ? actionData?.errors?.sender:"Anos Positos"}
-                color={actionData?.errors?.sender ? "warning":"neutral"}
-                variant="outlined"
-                value={sender}
-                onChange={(newValue) => { setSender(newValue.target.value) }}
-            />
-            <Box textAlign='center'>
-                <Button
-                    color="info"
-                    size="sm"
-                    sx={{ mr: 2, width: 300 }}
-                    onClick={() => {
-                        submit({ name, transportCompany, trackingNumber, sender }, { method: "post" })
-                    }}
-                >
-                    <FiSave style={{ marginRight: 2 }} />
-                    <Typography>Save</Typography>
-                </Button>
-            </Box>
-        </Stack>
-    );
+  return (
+    <Stack direction="column" spacing={3} sx={{ marginTop: 2 }}>
+      <TextField
+        label="Parcel Name"
+        placeholder={
+          actionData?.errors?.name
+            ? actionData?.errors?.name
+            : "iPhone 14 Pro Max"
+        }
+        color={actionData?.errors?.name ? "warning" : "neutral"}
+        variant="outlined"
+        value={name}
+        onChange={(newValue) => {
+          setName(newValue.target.value);
+        }}
+      />
+      <TextField
+        label="Tracking Number"
+        placeholder={
+          actionData?.errors?.trackingNumber
+            ? actionData?.errors?.trackingNumber
+            : "AB01234567890"
+        }
+        color={actionData?.errors?.trackingNumber ? "warning" : "neutral"}
+        variant="outlined"
+        value={trackingNumber}
+        onChange={(newValue) => {
+          setTrackingNumber(newValue.target.value);
+        }}
+      />
+      <TextField
+        label="Transport Company"
+        placeholder={
+          actionData?.errors?.transportCompany
+            ? actionData?.errors?.transportCompany
+            : "The Flash"
+        }
+        color={actionData?.errors?.transportCompany ? "warning" : "neutral"}
+        variant="outlined"
+        value={transportCompany}
+        onChange={(newValue) => {
+          setTransportCompany(newValue.target.value);
+        }}
+      />
+      <TextField
+        label="Sender"
+        placeholder={
+          actionData?.errors?.sender
+            ? actionData?.errors?.sender
+            : "Anos Positos"
+        }
+        color={actionData?.errors?.sender ? "warning" : "neutral"}
+        variant="outlined"
+        value={sender}
+        onChange={(newValue) => {
+          setSender(newValue.target.value);
+        }}
+      />
+      <Box textAlign="center">
+        <Button
+          color="info"
+          size="sm"
+          sx={{ mr: 2, width: 300 }}
+          onClick={() => {
+            submit(
+              { name, transportCompany, trackingNumber, sender },
+              { method: "post" }
+            );
+          }}
+        >
+          <FiSave style={{ marginRight: 2 }} />
+          <Typography>Save</Typography>
+        </Button>
+      </Box>
+    </Stack>
+  );
 }
-
